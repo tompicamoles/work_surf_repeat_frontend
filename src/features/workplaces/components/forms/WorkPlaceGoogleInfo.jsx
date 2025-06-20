b5e00b8057fe3849848014cafc2943470bb84fe7
tompayfit <108389751+tompayfit@users.noreply.github.com>
2025-06-20 18:22:52 +0200

    small  ui improvement

--- src/features/workplaces/components/forms/WorkPlaceGoogleInfo.jsx
+++ src/features/workplaces/components/forms/WorkPlaceGoogleInfo.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { LocationOn } from "@mui/icons-material";
+import { Box, Typography } from "@mui/material";
 import { useMapsLibrary } from "@vis.gl/react-google-maps";
 import { useEffect } from "react";
 
@@ -26,7 +27,12 @@ export const WorkPlaceGoogleInfo = ({ id, savePlaceDetails, formData }) => {
   return (
     <>
       <Typography variant="h5"> {formData.name} </Typography>
-      <Typography variant="h7">{formData.adress}</Typography>
+      <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+        <LocationOn sx={{ fontSize: 18, color: "text.secondary", mr: 0.5 }} />
+        <Typography variant="body2" color="text.secondary">
+          {formData.adress}
+        </Typography>
+      </Box>
     </>
   );
 };
